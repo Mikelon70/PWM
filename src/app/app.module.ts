e7802d34e25af49244a2545ee236361eaa39c43f
alber <albertitosocas@hotmail.com>
2022-04-04 10:24:05 +0100

    ejemplo 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {NoticiasList} from "./otras-noticias/otras-noticias.component";
+import { CajanoticiasComponent } from './cajanoticias/cajanoticias.component';
+import { NoticiasdestacadasComponent } from './noticiasdestacadas/noticiasdestacadas.component';
+import { EnlacesComponent } from './enlaces/enlaces.component';
+import { AvisosComponent } from './avisos/avisos.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: CajanoticiasComponent},
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    CajanoticiasComponent,
+    NoticiasList,
+    NoticiasdestacadasComponent,
+    EnlacesComponent,
+    AvisosComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
